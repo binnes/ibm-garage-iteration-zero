67d9c9f870c287370c36aadd6d207069dc88d527
Brian Innes <binnes@uk.ibm.com>
2020-12-08 12:55:55 +0000

    fixed variable name

--- terraform/stages-minikube/stage2-argocd.tf
+++ terraform/stages-minikube/stage2-argocd.tf
@@ -6,7 +6,7 @@ module "dev_tools_argocd" {
   olm_namespace       = module.dev_software_olm.olm_namespace
   operator_namespace  = module.dev_software_olm.target_namespace
   ingress_subdomain   = var.ingress_hostname
-  tls_secret          = var.tls_secret
+  tls_secret_name     = var.tls_secret
   app_namespace       = module.dev_tools_namespace.name
   name                = "argocd"
 }
